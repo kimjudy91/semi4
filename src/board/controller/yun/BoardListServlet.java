1a37d6ef3def20fad06f558a7e99a75066fe31ac
yun <cyh89221@naver.com>
2019-10-10 14:57:32 +0900

    test

--- src/board/controller/yun/BoardListServlet.java
+++ src/board/controller/yun/BoardListServlet.java
@@ -16,8 +16,24 @@ public class BoardListServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
+		String spageNum=req.getParameter("pageNum");
+		String field=req.getParameter("field");
+		String keyword=req.getParameter("keyword");
+		int pageNum=1;
+		if(spageNum!=null) {
+			pageNum=Integer.parseInt(spageNum);
+		}
+		int endRow=pageNum*10;
+		int startRow=endRow-9;
 		BoardDao dao=BoardDao.getinstance();
-		ArrayList<BoardVo> list=dao.list();
+		ArrayList<BoardVo> list=dao.list(startRow, endRow, field, keyword);
+		int pageCount=(int)Math.ceil(dao.getCount(field,keyword)/10.0);
+		int startPageNum=((pageNum-1)/10*10)+1;
+		int endPageNum=startPageNum+9;
+		if(endPageNum>pageCount) {
+			endPageNum=pageCount;
+		}
+		
 		req.setAttribute("list", list);
 		req.setAttribute("top","/board/header.jsp");
 		req.setAttribute("content","/board/community.jsp");
