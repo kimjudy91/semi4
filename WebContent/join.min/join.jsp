a31d2a2650304707d9cd752413dc38cf250e64af
lhmin56 <56283175+lhmin56@users.noreply.github.com>
2019-10-10 17:18:08 +0900

    ad

--- WebContent/join.min/join.jsp
+++ WebContent/join.min/join.jsp
@@ -1,11 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>join.html</title>
+<title>join.jsp</title>
 </head>
 <body>
-
 <link rel="stylesheet" type="text/css" href="../css.min/style.css"> 
 </head>
 <body>
@@ -20,19 +21,14 @@
 	<label>비밀번호</label><input type="password" name="pwd1"><br>
 	<label>비밀번호확인</label><input type="password" name="pwd2" onkeyup="checkOk()">
 	<span id="pwdcheck"></span><br>
-	<label>생년월일</label><input type="text" name="birth"><br>
+	<label>주민등록번호</label><input type="text" name="jumin"><br>
 	<label>전화번호</label><input type="text" name="phone" onkeyup="checkOk()">
 	<span id="phonecheck"></span><br>
-	<label>주소</label><input type="text" name="addr"><br>
-	<label>이메일</label><input type="text" name="eamil" onkeyup="checkOk()">
+	<label>주소</label><input type="text" name="address"><br>
+	<label>이메일</label><input type="text" name="email" onkeyup="checkOk()">
 	<span id="emailcheck"></span><br><br>
 </span>
-<span id="fav">
-	<label id="favjanre">좋아하는 음악장르</label>
-	<input type="checkbox" id="box"><label id="janre">Rock</label>
-	<input type="checkbox" id="box"><label id="janre">포크</label>
-	<input type="checkbox" id="box"><label id="janre">R&B</label>
-</span><br><br>
+
 	<input type="submit" value="회원가입" id="a">
 	<input type="reset" value="회원가입취소" id="a">
 	</div>
---
+++ src/joinController/min/JoinServlet.java
@@ -0,0 +1,43 @@
+package joinController.min;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import members.dao.min.MembersDao;
+import members.vo.min.MembersVo;
+
+@WebServlet("/join/insert")
+public class JoinServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendRedirect(req.getContextPath() + "/join.min/join.jsp");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		req.setCharacterEncoding("utf-8");
+		String id=req.getParameter("id");
+		String pwd=req.getParameter("pwd1");
+		String name=req.getParameter("name");
+		String jumin=req.getParameter("jumin");
+		String email=req.getParameter("email");
+		String address=req.getParameter("address");
+		String phone=req.getParameter("phone");
+		MembersVo vo=new MembersVo(id, pwd, name, jumin, email, address, phone, 0, 0, 1, 0);
+		MembersDao dao=new MembersDao();
+		int n=dao.insert(vo);
+		if(n>0) {
+			req.setAttribute("code", "success");
+		}else {
+			req.setAttribute("code", "fail");
+		}
+		req.getRequestDispatcher("/join.min/joinOk.jsp").forward(req, resp);
+		
+	}
+}
