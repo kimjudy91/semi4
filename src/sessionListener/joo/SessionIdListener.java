4f8297d5650f090f2e764aca8457d98c05a8d731
kimjudy91 <suizka81@naver.com>
2019-10-14 20:36:09 +0900

    zz

---
+++ src/sessionListener/joo/SessionIdListener.java
@@ -0,0 +1,36 @@
+package sessionListener.joo;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+import org.apache.catalina.SessionEvent;
+
+public class SessionIdListener implements HttpSessionAttributeListener {
+	public SessionIdListener() {}
+	String id;
+	private static ArrayList<String> IdList=new ArrayList<String>();
+	public static ArrayList<String> getUserId() {
+		return IdList;
+	}
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent event) {
+		HttpSession session=event.getSession();
+	    id=(String)session.getAttribute("id");
+		IdList.add(id);
+	}
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent event) {
+		HttpSession session=event.getSession();
+		//id=(String)session.getAttribute("id");
+		IdList.remove(id);
+		System.out.println(id);
+	}
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent event) {
+		
+		
+	}
+}
